dffb7ebbe96c3065a1417b1e10bf8f3f13edd53a
JhonyDalcin <jhonydalcin@gmail.com>
2022-09-27 06:18:24 -0300

    Project Created

---
+++ src/test/java/com/dalcin/javacourse/JavaCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalcin.javacourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
